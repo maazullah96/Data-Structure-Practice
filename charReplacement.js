9e64c7d246b247bf0989398159a7b733b7b65f09
maazullah96 <maazullahminhas96@gmail.com>
2023-08-23 08:55:56 +0500

    consecutive Ones

--- charReplacement.js
+++ charReplacement.js
@@ -7,16 +7,16 @@ var characterReplacement = function (s, k) {
   console.log(s, k)
   let winStart = 0
   let max = 0
-  let hash_map = {}
+  let hashMap = {}
   let maxFreq = 0
   for (let winEnd = 0; winEnd < s.length; winEnd++) {
     let char = s[winEnd]
-    hash_map[char] = hash_map[char] + 1 || 1
-    maxFreq = Math.max(hash_map[char], maxFreq)
+    hashMap[char] = hashMap[char] + 1 || 1
+    maxFreq = Math.max(hashMap[char], maxFreq)
     let window = winEnd - winStart + 1
     let remaining = window - maxFreq
     if (remaining > k) {
-      hash_map[s[winStart]] -= 1
+      hashMap[s[winStart]] -= 1
       winStart += 1
     }
     max = Math.max(winEnd - winStart + 1, max)
---
+++ consecutiveOnes.js
@@ -0,0 +1,6 @@
+var findMaxConsecutiveOnes = function (nums) {
+  console.log(nums)
+}
+
+nums = [1, 1, 0, 1, 1, 1]
+findMaxConsecutiveOnes(nums)
