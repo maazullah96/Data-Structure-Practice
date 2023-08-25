9b72826946ccdbfddbffadd070f8c0898d6854a7
maazullah96 <maazullahminhas96@gmail.com>
2023-08-25 23:36:39 +0500

    permutations tried but not completed

--- repl.js
+++ repl.js
@@ -1,6 +1,5 @@
 console.log(Math.max(1, 5))
 
-temp
 facebook = 'localhost'
 facebook = ''
 console.log(facebook || '/')
@@ -11,3 +10,13 @@ const temp = [1, 2, 3, 4, 5]
 
 const set = new Set(['a', 'b'])
 console.log(temp)
+temp.pop()
+temp
+
+temp.s
+
+s = { a: 1, b: 2, c: '3' }
+s
+console.log(s)
+
+console.log(s['a'])
