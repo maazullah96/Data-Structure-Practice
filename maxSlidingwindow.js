06fa81a8a9db0553e40a3f54d9d73c80a816f14e
maazullah96 <maazullahminhas96@gmail.com>
2023-09-07 22:39:42 +0500

    maxSlidingWindow Completed

--- maxSlidingwindow.js
+++ maxSlidingwindow.js
@@ -1,43 +1,42 @@
-
-class Queue {
-  // or enqueue, adding element n to the end of the line
-  push(n) {}
-
-  // or dequeue, remove the leader element
-  pop() {}
-}
-
-class MontonicQueue{
-  push(n){
-
-  }
-  pop(n){
-
-  }
-  max(){
-
-  }
-}
-
 const maxSlidingWindow = (nums, k) => {
-  let winStart = 0;
-  let window = []
-  for (let winEnd = 0; winEnd < nums.length; winEnd++) {
-    const element = nums[winEnd];
-    element
-    if (winEnd - winStart + 1 == k) {
+    let queue = []
+    let winStart =0
+    let result = []
+
+    for(let winEnd =0; winEnd< nums.length;winEnd++){
+      const num = nums[winEnd];
+      console.log(queue)
+      while (queue.length > 0 && num > nums[queue[queue.length - 1]]){
+        queue.pop()
+      }
+      num
+      // 
+      // num
+      // winStart
+      while (queue[0] < winStart) {
+        queue.shift()
+      }
+      
+      queue.push(winEnd);
+      
+      if(winEnd-winStart+1 === k){
+        result.push(nums[queue[0]]);
+        result
+        winStart+=1
+      }
+     
     }
-  }
+    result
+    return result
 };
-// nums = [1, 3, -1, -3, 5, 3, 6, 7]
+
+// nums = [1, 3, -1, -3,  5, 3, 6, 7];
 // k = 3
-let nums = [1, -1]
+
+let nums = [1, -1];
 let k = 1
 
-nums = [1, 3, -1, -3, 5, 3, 6, 7]
-k = 3
 const result = maxSlidingWindow(nums, k)
-result
 
 
 
